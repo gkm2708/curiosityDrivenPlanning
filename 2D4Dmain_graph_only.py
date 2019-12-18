41b0f3101b5bf97125a835ecb25d6cd0b07c5090
Gaurav Kumar <gkumar@techfak.uni-bielefeld.de>
2019-12-18 22:31:14 +0100

    added graph only

--- 2D4Dmain_graph_only.py
+++ 2D4Dmain_graph_only.py
@@ -10,12 +10,16 @@ np.set_printoptions(threshold=np.inf)
 
 class Vertex:
     def __init__(self, node, mat):
+        #print("Creating Vertex \n", mat)
         self.id = node
         self.mcv = mat
         self.adjacent = {}
 
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+
+    def get_adjacent(self):
+        return [x.id for x in self.adjacent]
 
     def add_neighbor(self, neighbor, weight=0):
         self.adjacent[neighbor] = weight
@@ -27,9 +31,11 @@ class Vertex:
         return self.id
 
     def get_mcv(self):
+        #print("While getting \n",self.mcv)
         return self.mcv
 
     def set_mcv(self, tempMCV):
+        #print("While setting \n",tempMCV)
         self.mcv = tempMCV
 
     def get_weight(self, neighbor):
@@ -47,6 +53,7 @@ class Graph:
         return iter(self.vert_dict.values())
 
     def add_vertex(self, node, mat):
+        #print("Inside add vertex \n", mat)
         self.num_vertices = self.num_vertices + 1
         new_vertex = Vertex(node, mat)
         self.vert_dict[node] = new_vertex
@@ -60,6 +67,7 @@ class Graph:
 
     def get_vertex_mcv(self, n):
         if n in self.vert_dict:
+            #print("get vertex mcv \n", self.vert_dict[n].get_mcv())
             return self.vert_dict[n].get_mcv()
         else:
             return None
@@ -67,11 +75,11 @@ class Graph:
     def set_vertex_mcv(self, n, tempMCV):
         self.vert_dict[n].set_mcv(tempMCV)
 
-    def add_edge(self, frm, to, cost = 0):
+    def add_edge(self, frm, frm_mat, to, to_mat, cost = 0):
         if frm not in self.vert_dict:
-            self.add_vertex(frm)
+            self.add_vertex(frm, frm_mat)
         if to not in self.vert_dict:
-            self.add_vertex(to)
+            self.add_vertex(to, to_mat)
 
         self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
         self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
@@ -90,14 +98,17 @@ class Graph:
                 print('( %s , %s, %s)'  % ( vid, wid, v.get_weight(w)))
 
         """
-        """
+
+        #"""
         for v in g:
             print('g.vert_dict[%s]=%s' %(v.get_id(), g.vert_dict[v.get_id()]))
-        """
+        #"""
 
+        """
         for v in g:
             vid = v.get_id()
             print('( %s , \n%s)' % (vid, v.get_mcv()))
+        """
 
     def plotMap2D(self, t, step, bDirection, fDirection):
 
@@ -375,25 +386,36 @@ def plan2D():
     best = unique[0][-1]
     count = unique[1][-1]
 
-    # find if highest curiosity has multiple counts
-    # if yes : action random
-    # acts as a tiebreaker for initialized MCV and randomize exact move in the direction
-    if count > 1:
-        searchIndex = 0
-        actionIndex = random.randint(1, count)
-        for i in range(0, 5):
-            for j in range(0, 5):
-                if tempMCV[i][j] == best:
-                    searchIndex += 1
-                if searchIndex == actionIndex:
-                    move_x = i
-                    move_y = j
-                    break
-            if searchIndex == actionIndex:
-                break
-    else:  # else  : max curiosity
-        move_x, move_y = np.unravel_index(tempMCV.argmax(), tempMCV.shape)
 
+    # check if there are multiple curiosities to mark nodes that have all its neighbours visited
+
+    if unique[0].shape[0] > 1:
+        # find if highest curiosity has multiple counts
+        # if yes : action random
+        # acts as a tiebreaker for initialized MCV and randomize exact move in the direction
+        if count > 1:
+            searchIndex = 0
+            actionIndex = random.randint(1, count)
+            for i in range(0, 5):
+                for j in range(0, 5):
+                    if tempMCV[i][j] == best:
+                        searchIndex += 1
+                    if searchIndex == actionIndex:
+                        move_x = i
+                        move_y = j
+                        break
+                if searchIndex == actionIndex:
+                    break
+        else:  # else  : max curiosity
+            move_x, move_y = np.unravel_index(tempMCV.argmax(), tempMCV.shape)
+    else:
+        print("\nonly zero curiosity left at this node : find a connected node with non zero value")
+        for v in g:
+            if str(v.get_id()) == vertex:
+                temp = v.get_adjacent()
+                for i in range(0, len(temp)):
+                    print(temp[i])
+        exit(0)
 
 
 
@@ -443,6 +465,7 @@ def initializeMCV2D(t, step):
     if boundarySwitch:
         tempMCV = tempMCV * boundarymask
 
+    #print("Initialize \n",tempMCV)
     g.add_vertex(vertex, tempMCV)
 
 
@@ -471,6 +494,8 @@ def updateMCV2D():
         vertex_previous = "(" + str(previous_x) + "," + str(previous_y) + "," + str(goal_x) + "," + str(goal_y) + ")"
 
     tempMCV_agent = g.get_vertex_mcv(vertex_agent)
+    #print("Inside Update\n",tempMCV_agent)
+
     tempMCV_previous = g.get_vertex_mcv(vertex_previous)
 
     bMask = np.zeros((realgrid,realgrid))
@@ -481,23 +506,25 @@ def updateMCV2D():
 
     if previous_x <= agent_x:
         if previous_y <= agent_y:
-            bMask[previous_x:agent_x+1, previous_y:agent_y+1] = 0.99
+            bMask[previous_x:agent_x+1, previous_y:agent_y+1] = 0.0
             fMask[previous_x:agent_x+1, previous_y:agent_y+1] = 1.01
         else:
-            bMask[previous_x:agent_x+1, agent_y:previous_y+1] = 0.99
+            bMask[previous_x:agent_x+1, agent_y:previous_y+1] = 0.0
             fMask[previous_x:agent_x+1, agent_y:previous_y+1] = 1.01
     else:
         if previous_y <= agent_y:
-            bMask[agent_x:previous_x+1, previous_y:agent_y+1] = 0.99
+            bMask[agent_x:previous_x+1, previous_y:agent_y+1] = 0.0
             fMask[agent_x:previous_x+1, previous_y:agent_y+1] = 1.01
         else:
-            bMask[agent_x:previous_x+1, agent_y:previous_y+1] = 0.99
+            bMask[agent_x:previous_x+1, agent_y:previous_y+1] = 0.0
             fMask[agent_x:previous_x+1, agent_y:previous_y+1] = 1.01
 
     bDirection = bMask[agent_x-2:agent_x+3,agent_y-2:agent_y+3]
     fDirection = fMask[previous_x-2:previous_x+3,previous_y-2:previous_y+3]
 
     tempMCV_agent = tempMCV_agent * bDirection
+    #print(tempMCV_agent)
+
     tempMCV_previous = tempMCV_previous * fDirection
 
     # enable below to lower curiosity from backward direction
@@ -552,7 +579,6 @@ def manipulateGraph2D(t, step):
     if not found or g.num_vertices == 0:
         # node not found hence initialize MCV and find next action
         # or started with empty graph
-
         initializeMCV2D(t, step)
 
     return bDirection, fDirection
@@ -589,31 +615,60 @@ def rollout2D(t, max_steps):
     global bDirection
     global fDirection
 
+
+    # first step : initialize first node
+    #       else : check if intialization is needed or upate is needed
+    #               also add an edge
+    # plan for next node if curiosity guides
+    # else traverse to reach a node in graph with non zero curiosity
+
+
     for i in range(1, max_steps):
         bDirection, fDirection = manipulateGraph2D(t, i)
+
+        # assume that a next node is found
+        # add a link from previous to this with zero value on edge
+
+        if dimensionCount == 2:
+            vertex_agent = "(" + str(agent_x) + "," + str(agent_y) + ")"
+            vertex_previous = "(" + str(previous_x) + "," + str(previous_y) + ")"
+        elif dimensionCount == 4:
+            vertex_agent = "(" + str(agent_x) + "," + str(agent_y) + "," + str(goal_x) + "," + str(goal_y) + ")"
+            vertex_previous = "(" + str(previous_x) + "," + str(previous_y) + "," + str(goal_x) + "," + str(
+                goal_y) + ")"
+
+        if i > 1:
+            g.add_edge(vertex_previous, g.get_vertex_mcv(vertex_previous), vertex_agent, g.get_vertex_mcv(vertex_agent), 1)
+
+
+
 
         previous_x = agent_x
         previous_y = agent_y
 
-        plan2D()
-        g.plotMap2D(t, i, bDirection, fDirection)
 
-        # Not Moving : Out of Boundary
-        if 1 < agent_x + move_x -2 < realgrid - 2 and 1 < agent_y + move_y -2 < realgrid - 2:
-            agent_x = agent_x + move_x - 2
-            agent_y = agent_y + move_y - 2
+
+        # plan to find either non zero curiosity or a connected node with non zero curiosity
+        plan2D()
+
+        #g.plotMap2D(t, i, bDirection, fDirection)
+
+        agent_x = agent_x + move_x - 2
+        agent_y = agent_y + move_y - 2
+
+
 
         # If Goal Found
-        if 1 < agent_x < realgrid - 2 and 1 < agent_y < realgrid - 2:
-            if agent_x - 3 < goal_x < agent_x + 3 and agent_y - 3 < goal_y < agent_y + 3:
-                break
+        #if 1 < agent_x < realgrid - 2 and 1 < agent_y < realgrid - 2:
+        #    if agent_x - 3 < goal_x < agent_x + 3 and agent_y - 3 < goal_y < agent_y + 3:
+        #        break
 
         #time.sleep(10)
 
 
 if __name__ == '__main__':
 
-    max_episode = 100
+    max_episode = 1
     max_steps = 100
 
     # ###################################################################################
@@ -624,12 +679,12 @@ if __name__ == '__main__':
     boundarySwitch = True
 
     # enable below to update (Raise) MCV for previous position in the direction of movement
-    fDirectionSwitch = True
+    fDirectionSwitch = False
 
     # enable below to update (Lower) MCV for current position from the direction of movement
     bDirectionSwitch = True
 
-    dimensionCount = 4
+    dimensionCount = 2
 
     # Trial
     for t in trange(0, max_episode):
@@ -655,4 +710,4 @@ if __name__ == '__main__':
         # return when goal is found or 1000 steps
         rollout2D(t, max_steps)
 
-    g.printGraph()
+        g.printGraph()
