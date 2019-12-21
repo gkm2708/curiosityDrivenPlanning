a6151714665ba404a830dca9562394bfbb3d81e9
gkumar <gaurav.kumar@techfak.uni-bielefeld.de>
2019-12-21 17:50:45 +0100

    random maze generation added

--- 2D4Dmain_graph_only.py
+++ 2D4Dmain_graph_only.py
@@ -5,78 +5,9 @@ import os
 import time
 import cv2
 import math
+from maze import Maze
+
 np.set_printoptions(threshold=np.inf)
-
-
-
-# initialize grid
-"""
-grid = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,1,0,0,1,0,0,1,1,1,1,0,0],
-                [0,0,1,0,0,1,0,0,1,0,0,1,0,0],
-                [0,0,1,0,0,1,0,0,1,0,0,1,0,0],
-                [0,0,1,0,0,1,0,0,1,0,0,1,0,0],
-                [0,0,1,0,0,1,0,0,1,0,0,1,0,0],
-                [0,0,1,0,0,1,1,1,1,0,0,1,0,0],
-                [0,0,1,0,0,1,0,0,1,0,0,1,0,0],
-                [0,0,1,0,0,1,0,0,1,0,0,1,0,0],
-                [0,0,1,0,0,1,0,0,1,0,0,1,0,0],
-                [0,0,1,1,1,1,0,0,1,0,0,1,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
-"""
-
-
-
-grid = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,1,1,1,1,1,1,1,1,1,1,0,0],
-                [0,0,1,0,1,1,0,0,1,1,0,1,0,0],
-                [0,0,1,0,1,1,0,0,1,1,0,1,0,0],
-                [0,0,1,0,1,1,0,0,1,1,0,1,0,0],
-                [0,0,1,0,1,1,0,0,1,1,0,1,0,0],
-                [0,0,0,0,1,0,0,0,0,1,0,0,0,0],
-                [0,0,1,0,1,1,0,0,1,1,0,1,0,0],
-                [0,0,1,0,1,1,0,0,1,1,0,1,0,0],
-                [0,0,1,0,1,1,0,0,1,1,0,1,0,0],
-                [0,0,1,1,1,1,1,1,1,1,1,1,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
-
-"""
-# initialize grid
-smallGrid = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,1,0,0,1,0,0,1,1,1,1,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,1,1,1,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,1,1,1,1,0,0,1,0,0,1,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
-                    
-"""
-
-smallGrid = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,1,1,1,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
 
 
 boundarymask = np.array([[1, 1, 1, 1, 1],
@@ -84,21 +15,22 @@ boundarymask = np.array([[1, 1, 1, 1, 1],
                         [1, 0, 0, 0, 1],
                         [1, 0, 0, 0, 1],
                         [1, 1, 1, 1, 1]])
-
-"""
-initMCV = np.array([[1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1]])
-                    
-"""
-
+                        
 initMCV = np.array([[255, 255, 255, 255, 255],
                     [255, 255, 255, 255, 255],
                     [255, 255, 255, 255, 255],
                     [255, 255, 255, 255, 255],
                     [255, 255, 255, 255, 255]])
+"""
+
+boundarymask = np.array([[1, 1, 1],
+                        [1, 0, 1],
+                        [1, 1, 1]])
+
+initMCV = np.array([[255, 255, 255],
+                    [255, 255, 255],
+                    [255, 255, 255]])
+"""
 
 
 
@@ -222,7 +154,7 @@ class Graph:
 
         cv2.imwrite("2d/curiosity_map.png", imageExpanded)
 
-    def plotTrajectory(self, trajectory, t):
+    def plotTrajectory(self, trajectory, m, t):
 
         imageMCV = np.asarray([[0 if item == 0 else 255 for item in row] for row in grid])
         imageExpanded = np.zeros((realgrid*7,realgrid*7))
@@ -246,9 +178,9 @@ class Graph:
                     for jj in range(0, 7):
                         imageExpanded[i * 7 + ii][j * 7 + jj] = imageMCV[i][j]
 
-        cv2.imwrite("2d/trajectory_map_"+str(t)+".png", imageExpanded)
+        cv2.imwrite("2d/"+str(m)+"/trajectory_map_"+str(t)+".png", imageExpanded)
 
-    def plotGraphBuilding(self, t):
+    def plotGraphBuilding(self, t, m):
 
         imageGrid = np.asarray([[0 if item == 0 else 255 for item in row] for row in grid])
         imagemotion = np.asarray([[0 if item == 0 else 255 for item in row] for row in grid])
@@ -268,7 +200,8 @@ class Graph:
 
             imageMCV[x][y] = 100
 
-            imageFog[x-2:x+3,y-2:y+3] = imageGrid[x-2:x+3,y-2:y+3]
+            imageFog[x-int(fovea/2):x+int(fovea/2)+1,y-int(fovea/2):y+int(fovea/2)+1] = imageGrid[x-int(fovea/2):x+int(fovea/2)+1,
+                                                                                        y-int(fovea/2):y+int(fovea/2)+1]
 
         imagemotion[previous_x, previous_y] = 200
         imagemotion[agent_x, agent_y] = 100
@@ -284,9 +217,20 @@ class Graph:
                     for jj in range(0, 7):
                         imageExpanded[i * 7 + ii][j * 7 + jj] = imageFull[i][j]
 
-        cv2.imwrite("2d/generated_map_"+str(t)+".png", imageExpanded)
+        #print("2d/"+str(m)+"/generated_map_"+str(t)+".png")
+        cv2.imwrite("2d/"+str(m)+"/generated_map_"+str(t)+".png", imageExpanded)
+
 
 g = Graph()
+
+# ######################################## GRID CONSTRUCTION #####################################
+x = Maze()
+
+grid = x.generateMaze(10,10,1)
+offset_x = np.zeros((1,grid.shape[0]))
+grid = np.concatenate([offset_x, grid, offset_x], axis=0)
+offset_y = np.zeros((grid.shape[1]+2,1))
+grid = np.concatenate([offset_y, grid, offset_y], axis=1)
 
 realgrid = grid.shape[0]
 
@@ -296,10 +240,13 @@ goal_x, goal_y = 0, 0
 previous_x, previous_y = 0, 0
 next_x, next_y = 0, 0
 
+fovea = 5
+
 bDirection = np.zeros((realgrid, realgrid))
 fDirection = np.zeros((realgrid, realgrid))
 fog = np.zeros((realgrid, realgrid))
-reachability = np.zeros((5, 5))
+
+reachability = np.zeros((fovea, fovea))
 
 traversal_status = {}
 graph = {}
@@ -309,18 +256,11 @@ fDirectionSwitch = False
 bDirectionSwitch = False
 
 
-def randomValidPoint(grid_type):
-
-    if grid_type == "SmallGrid":
-        x, y = 0, 0
-        while smallGrid[x][y] == 0:
-            x = random.randint(1, realgrid - 2)
-            y = random.randint(1, realgrid - 2)
-    elif grid_type == "FullGrid":
-        x, y = 0, 0
-        while grid[x][y] == 0:
-            x = random.randint(1, realgrid - 2)
-            y = random.randint(1, realgrid - 2)
+def randomValidPoint():
+    x, y = 0, 0
+    while grid[x][y] == 0:
+        x = random.randint(1, realgrid - 2)
+        y = random.randint(1, realgrid - 2)
     return x, y
 
 
@@ -352,24 +292,26 @@ def initializeMCV2D(point_x, point_y):
     global reachability
 
     # hardcoded reachability discovery from grid
-    reachability = np.zeros((5, 5))
+    reachability = np.zeros((fovea, fovea))
 
     tempGrid = np.empty_like(grid)
-    tempGrid[:] = grid
+    tempGrid[:] = grid[:]
 
-    reachability = np.asarray(tempGrid[point_x-2:point_x+3, point_y-2:point_y+3])
+    reachability = np.asarray(tempGrid[point_x-int(fovea/2):point_x+int(fovea/2)+1, point_y-int(fovea/2):point_y+int(fovea/2)+1])
 
-    floodfill(2, 2)
+    floodfill(int(fovea/2), int(fovea/2))
 
     reachability = np.asarray([[1 if item == 2 else 0 for item in row] for row in reachability])
 
     temp = np.empty_like(fog)
     temp[:,:] = fog[:,:]
 
-    for i in range(0,5):
-        for j in range(0,5):
+    for i in range(0,fovea):
+        for j in range(0,fovea):
             if reachability[i,j] == 1:
-                temp[point_x-2+i,point_y-2+j] = reachability[i,j]
+                temp[point_x-int(fovea/2)+i,point_y-int(fovea/2)+j] = reachability[i,j]
+
+    #print(point_x, point_y, "\n", reachability)
 
     if np.array_equal(temp, fog):
         print("No new area uncovered")
@@ -382,6 +324,7 @@ def initializeMCV2D(point_x, point_y):
         tempMCV = initMCV * reachability
         if boundarySwitch:
             tempMCV = tempMCV * boundarymask
+        #print(" MCV \n",tempMCV)
 
         # add vertex and set traversal status
         vertex = "(" + str(point_x) + "," + str(point_y) + ")"
@@ -421,7 +364,7 @@ def updateMCV2D():
 
     # enable below to lower curiosity from backward direction
     if bDirectionSwitch and type(tempMCV_agent) is np.ndarray:
-        bDirection = bMask[agent_x-2:agent_x+3,agent_y-2:agent_y+3]
+        bDirection = bMask[agent_x-int(fovea/2):agent_x+int(fovea/2)+1,agent_y-int(fovea/2):agent_y+int(fovea/2)+1]
         tempMCV_agent = tempMCV_agent * bDirection
         g.set_vertex_mcv(vertex_agent, tempMCV_agent)
         unique = np.unique(tempMCV_agent, return_counts=True)
@@ -430,7 +373,7 @@ def updateMCV2D():
 
     # enable below to raise curiosity in forward direction
     if fDirectionSwitch and type(tempMCV_previous) is np.ndarray:
-        fDirection = fMask[previous_x-2:previous_x+3,previous_y-2:previous_y+3]
+        fDirection = fMask[previous_x-int(fovea/2):previous_x+int(fovea/2)+1,previous_y-int(fovea/2):previous_y+int(fovea/2)+1]
         tempMCV_previous = tempMCV_previous * fDirection
         g.set_vertex_mcv(vertex_previous, tempMCV_previous)
         unique = np.unique(tempMCV_previous, return_counts=True)
@@ -474,6 +417,7 @@ def plan2D():
     for v in traversal_status:
         if traversal_status[v] == "Remaining":
             findStatus = findaction(v)
+            #print(findStatus)
             if findStatus:
                 vertex = g.get_vertex(v)
                 vid = vertex.get_id()
@@ -505,22 +449,20 @@ def findaction(vertex):
         if count > 1:
             searchIndex = 0
             actionIndex = random.randint(1, count)
-            for i in range(0, 5):
-                for j in range(0, 5):
+            for i in range(0, fovea):
+                for j in range(0, fovea):
                     if tempMCV[i][j] == best:
                         searchIndex += 1
                     if searchIndex == actionIndex:
                         move_x, move_y = i, j
                         return True
-                    return False
         else:  # else  : max curiosity
             move_x, move_y = np.unravel_index(tempMCV.argmax(), tempMCV.shape)
             return True
-    else:
-        return False
+    #return False
 
 
-def buildGraph(max_steps):
+def buildGraph(max_steps, m):
 
     global previous_x
     global previous_y
@@ -533,7 +475,7 @@ def buildGraph(max_steps):
 
     for i in range(1, max_steps):
 
-        g.plotGraphBuilding(i)
+        g.plotGraphBuilding(i, m)
 
         if i == 1:
             initializeMCV2D(previous_x, previous_y)
@@ -545,7 +487,7 @@ def buildGraph(max_steps):
             break
 
         previous_x, previous_y = next_x, next_y
-        agent_x, agent_y = previous_x + move_x -2, previous_y + move_y -2
+        agent_x, agent_y = previous_x + move_x -int(fovea/2), previous_y + move_y -int(fovea/2)
 
         manipulateGraph2D()
 
@@ -648,6 +590,7 @@ def planTrajectory():
 
     trajectory.append("("+str(goal_x)+","+str(goal_y)+")")
     trajectory.append(x)
+
     while True:
         x = adj_node[x]
         if x is None:
@@ -668,7 +611,7 @@ def planTrajectory():
 
 if __name__ == '__main__':
 
-    max_episode, max_steps = 10, 1000
+    maze_trials, max_episode, max_steps = 1, 10, 1000
 
     # ###################################################################################
     # ################################### IMPORTANT #####################################
@@ -683,28 +626,34 @@ if __name__ == '__main__':
     # enable below to update (Lower) MCV for current position from the direction of movement
     bDirectionSwitch = True
 
-    agent_x, agent_y = randomValidPoint("FullGrid")
+    for m in range(0, maze_trials):
 
-    # set history at initilisation
-    previous_x, previous_y = agent_x, agent_y
-
-    # return when goal is found or 1000 steps
-    buildGraph(max_steps)
-
-    initial_graph()
-
-    print("GRAPH\n", graph)
-
-    for t in trange(0, max_episode):
-
-        agent_x, agent_y = randomValidPoint("FullGrid")
-        goal_x, goal_y = randomValidPoint("FullGrid")
+        agent_x, agent_y = randomValidPoint()
 
         # set history at initilisation
         previous_x, previous_y = agent_x, agent_y
 
+        if not os.path.exists("2d/"+str(m)):
+            os.makedirs("2d/"+str(m))
+
         # return when goal is found or 1000 steps
-        trajectory = planTrajectory()
-        print("The path between ", "(" + str(agent_x) + "," + str(agent_y) + ")", " to ",
-              "(" + str(goal_x) + "," + str(goal_y) + ")", trajectory)
-        g.plotTrajectory(trajectory, t)
+        buildGraph(max_steps, m)
+        initial_graph()
+
+        print("GRAPH\n", graph)
+
+        for t in trange(0, max_episode):
+
+            agent_x, agent_y = randomValidPoint()
+            goal_x, goal_y = randomValidPoint()
+
+            # set history at initilisation
+            previous_x, previous_y = agent_x, agent_y
+
+            # return when goal is found or 1000 steps
+            trajectory = planTrajectory()
+
+            print("The path between ", "(" + str(agent_x) + "," + str(agent_y) + ")", " to ",
+                "(" + str(goal_x) + "," + str(goal_y) + ")", trajectory)
+
+            g.plotTrajectory(trajectory, m, t)
